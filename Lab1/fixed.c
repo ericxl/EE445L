d03ad1b66ec22df49fdb37ffbd0eeee2d321ee6a
Eric Liang <jklelxy@gmail.com>
2017-09-07 13:38:20 -0500

    initial commit

---
+++ Lab1/fixed.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include "ST7735.h"
+
+
+void ST7735_sDecOut3(int32_t n)
+{
+	
+}
+
+
+void ST7735_uBinOut8(uint32_t n)
+{
+	
+}
+
+void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, int32_t maxY)
+{
+	
+}
+
+
+void ST7735_XYplot(uint32_t count, int32_t bufX[], int32_t bufY[])
+{
+	
+}
