dc3cbe23197108ca71bd5f75a61a6b1ee2fcac83
Eric Liang <jklelxy@gmail.com>
2017-09-07 14:09:44 -0500

    draw pixel done

--- Lab1/fixed.c
+++ Lab1/fixed.c
@@ -66,7 +66,7 @@ void ST7735_uBinOut8(uint32_t n)
 }
 
 
-volatile int MinX, MaxX, MinY, MaxY;
+volatile int32_t MinX, MaxX, MinY, MaxY;
 
 void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, int32_t maxY)
 {
@@ -80,20 +80,17 @@ void ST7735_XYplotInit(char *title, int32_t minX, int32_t maxX, int32_t minY, in
   ST7735_OutString(title);
 }
 
-
 void ST7735_XYplot(uint32_t count, int32_t bufX[], int32_t bufY[])
 {
   for (int i = 0; i < count; ++i) {
     int x = bufX[i];
     int y = bufY[i];
 		
-		if (x < MinX || MaxX < x || y < MinY || MaxY < y) {
-			continue;
+		if (x <= MaxX && x >= MinX && y <= MaxY && y >= MinY){
+			int hwX = (127 * (x - MinX)) / (MaxX - MinX);
+			int hwY = 32 + (127 * (MaxY - y)) / (MaxY - MinY);
+			
+			ST7735_DrawPixel(hwX, hwY, ST7735_MAGENTA);
 		}
-		
-    int hwX = (127 * (x - MinX)) / (MaxX - MinX);
-    int hwY = 32 + (127 * (MaxY - y)) / (MaxY - MinY);
-		
-    ST7735_DrawPixel(hwX, hwY, ST7735_MAGENTA);
   }
 }
